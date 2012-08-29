ef037819bef741996a6bb90f424f2547d7709692
Fernando Takai <fernando.takai@gmail.com>
2012-08-29 13:52:35 -0300

    Setup requires requires nose now.

--- setup.py
+++ setup.py
@@ -12,10 +12,12 @@ setup(
     url="https://github.com/pculture/babelsubs",
     license='LICENSE.txt',
     packages=['babelsubs'],
+    setup_requires=['nose>=1.0'],
     install_requires=[
         'lxml==2.3',
         'html5lib==0.95',
         'wsgiref==0.1.2',
+        
     ],
     dependency_links=[
         'https://github.com/jsocol/bleachmastertarball/master#egg=bleach-dev'
