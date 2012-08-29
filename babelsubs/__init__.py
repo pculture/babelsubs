c307f4cbb696e7558ba743c37169ed0af0a7bb8b
Fernando Takai <fernando.takai@gmail.com>
2012-08-29 13:52:19 -0300

    If you pass a filepath to load_from, load it.

--- babelsubs/__init__.py
+++ babelsubs/__init__.py
@@ -1,12 +1,18 @@
+import os
 import parsers
 
 def load_from(sub_from, type=None, language=None):
+    if os.path.isfile(sub_from):
+        sub_from = open(sub_from)
+
     if hasattr(sub_from, 'read'):
         if type is None and not getattr(sub_from, 'name', None):
             raise TypeError("Couldn't find out the type by myself. Care to specify?")
 
         type = sub_from.name.split(".")[-1]
-        sub_from = sub_from.read()
+
+        with sub_from:
+            sub_from = sub_from.read()
     elif isinstance(sub_from, basestring) and type is None:
         raise TypeError("Couldn't find out the type by myself. Care to specify?")
 
