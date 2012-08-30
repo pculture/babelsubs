a30da7b9559ce4cde4bbd0e7e3cf6f9d6b0923e9
Fernando Takai <fernando.takai@gmail.com>
2012-08-30 11:53:09 -0300

    * Changed order of __init__ * Made format_time an instance method

--- babelsubs/generators/srt.py
+++ babelsubs/generators/srt.py
@@ -1,33 +1,33 @@
 from babelsubs.generators.base import BaseGenerator, register
 
-def format_srt_time(milliseconds):
-    seconds, milliseconds = divmod(int(milliseconds), 1000)
-    minutes, seconds = divmod(seconds, 60)
-    hours, minutes = divmod(minutes, 60)
-    return "%02i:%02i:%02i,%03i" % (hours, minutes, seconds, milliseconds)
 
 class SRTGenerator(BaseGenerator):
     file_type = 'srt'
 
-    def __init__(self, subtitle_set):
+    def __init__(self, subtitle_set, language=None):
+        super(SRTGenerator, self).__init__(subtitle_set, language)
         self.line_delimiter = '\r\n'
-        self.subtitle_set = subtitle_set
-        super(SRTGenerator, self).__init__(subtitle_set)
 
     def __unicode__(self):
         output = []
         i = 1
         # FIXME: allow formatting tags
-        for from_ms, to_ms, content in self.subtitle_set.subtitle_items(allow_format_tags=False):
+        for from_ms, to_ms, content in self.subtitle_set.subtitle_items(allow_format_tags=self.allows_formatting):
             output.append(unicode(i))
             output.append(u'%s --> %s' % (
-                format_srt_time(from_ms),
-                format_srt_time(to_ms)
+                self._format_srt_time(from_ms),
+                self._format_srt_time(to_ms)
             ))
             output.append(content)
             output.append(u'')
             i += 1
         return self.line_delimiter.join(output)
+
+    def _format_srt_time(self, milliseconds):
+        seconds, milliseconds = divmod(int(milliseconds), 1000)
+        minutes, seconds = divmod(seconds, 60)
+        hours, minutes = divmod(minutes, 60)
+        return "%02i:%02i:%02i,%03i" % (hours, minutes, seconds, milliseconds)
 
 
 register(SRTGenerator)
