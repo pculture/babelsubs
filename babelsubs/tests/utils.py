a184557bc892e9ff760d5d6d1b52c8adf3a686ff
Fernando Takai <fernando.takai@gmail.com>
2012-09-03 12:03:15 -0300

    Use load_from instead of discover

--- babelsubs/tests/utils.py
+++ babelsubs/tests/utils.py
@@ -4,12 +4,10 @@ General helpers needed by the other tests.
 
 import os
 
-from babelsubs import parsers
+import babelsubs
 
 def get_data_file_path(file_name):
     return os.path.join(os.path.dirname(__file__), 'data', file_name)
 
 def get_subs(file_name):
-    data = open(get_data_file_path(file_name)).read()
-    extension = os.path.splitext(file_name)[1][1:]
-    return parsers.discover(extension).parse(data)
+    return babelsubs.load_from(get_data_file_path(file_name))
