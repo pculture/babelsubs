3b24b2815bd4cdff0b9fbb07d5df8d9eff8683a7
Arthur Debert <arthur@stimuli.com.br>
2013-02-04 12:04:48 -0200

    Fixed linebreaks on generating txt, for amara #2010

--- babelsubs/generators/txt.py
+++ babelsubs/generators/txt.py
@@ -4,6 +4,14 @@ from babelsubs.generators.base import BaseGenerator, register
 class TXTGenerator(BaseGenerator):
     file_type = 'txt'
 
+    def __init__(self, subtitle_set, line_delimiter=u'\n\n', language=None):
+        """
+        Generator is list of {'text': 'text', 'start': 'seconds', 'end': 'seconds'}
+        """
+        self.subtitle_set = subtitle_set
+        self.line_delimiter = line_delimiter
+        self.language = language
+
     def __unicode__(self):
         output = []
         for _, _, content, _ in self.subtitle_set.subtitle_items():
