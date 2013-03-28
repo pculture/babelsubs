d2b503338781307b09e4ee40758d1c6c18976d31
Ben Dean-Kawamura <ben@pculture.org>
2013-03-28 14:15:36 -0400

    Use difflib for diffing.
    This allows us to find insertions and deletions between the 2
    sequences, instead of simply matching item by item.
    
    This changes subtitle_data.  Now it's possible to have empty
    SubtitleLines in the middle of the list, rather than only at the end.
    
    This also changes time_changed and text_changed:
    
    Before we calculated the number of differences using an item-by-item
    comparison, then divided that by the length of the longest sequence.
    
    Now we calculate using difflib's ratio() method.  ratio() calculates
    the number of differences, multiplies by 2, then divides by the sum of
    the 2 sequence lengths.
    
    This should be the same if the sequence lengths match up, but if items
    are inserted or deleted, then the numbers will be different.  I think
    this is fine, since before if you inserted/deleted a subtitle, the old
    calculation would give you a totally bogus number.

--- babelsubs/tests/test_diffing.py
+++ babelsubs/tests/test_diffing.py
@@ -1,5 +1,5 @@
 from unittest2 import TestCase
-from babelsubs.storage import SubtitleSet, diff
+from babelsubs.storage import SubtitleSet, SubtitleLine, diff
 
 class DiffingTest(TestCase):
     def test_empty_subs(self):
@@ -67,6 +67,43 @@ class DiffingTest(TestCase):
             self.assertEqual(sub_data['time_changed'], i ==1)
             self.assertFalse(sub_data['text_changed'])
 
+    def test_insert(self):
+        set_1 = SubtitleSet.from_list('en', [
+         (0, 1000, "Hey 1"),
+         (1000, 2000, "Hey 2"),
+         (2000, 3000, "Hey 3"),
+         (3000, 4000, "Hey 4"),
+        ])
+        set_2 = SubtitleSet.from_list('en', [
+         (0, 1000, "Hey 1"),
+         (500, 800, "Hey 1.5"),
+         (1000, 2000, "Hey 2"),
+         (2000, 3000, "Hey 3"),
+         (3000, 4000, "Hey 4"),
+        ])
+        result = diff(set_1, set_2)
+        self.assertEqual(result['changed'], True)
+        # for both time_change and text_changed, we calculate them as follows:
+        # there are 9 total subs (4 in set_1, 5 in set_2).  8 of those are
+        # matches and 1 is new in set_2.  So the change amount is 1/9
+        self.assertAlmostEqual(result['time_changed'], 1/9.0)
+        self.assertAlmostEqual(result['text_changed'], 1/9.0)
+        self.assertEqual(len(result['subtitle_data']), 5)
+
+        # check the lines that haven't changed
+        for i in (0, 2, 3, 4):
+            sub_data = result['subtitle_data'][i]
+            self.assertEquals(sub_data['time_changed'], False)
+            self.assertEquals(sub_data['text_changed'], False)
+            self.assertEquals(sub_data['subtitles'][0], set_2[i])
+            self.assertEquals(sub_data['subtitles'][1], set_2[i])
+        # check the line that was inserted
+        insert_sub_data = result['subtitle_data'][1]
+        self.assertEquals(insert_sub_data['time_changed'], True)
+        self.assertEquals(insert_sub_data['text_changed'], True)
+        self.assertEquals(insert_sub_data['subtitles'][0],
+                          SubtitleLine(None, None, None, None))
+        self.assertEquals(insert_sub_data['subtitles'][1], set_2[1])
 
     def test_data_ordering(self):
         set_1 = SubtitleSet.from_list('en', [
@@ -95,6 +132,4 @@ class DiffingTest(TestCase):
             ])
         result = diff(set_1, set_2)
 
-        self.assertEqual(result['time_changed'], 0.5)
-
-
+        self.assertAlmostEqual(result['time_changed'], 1/3.0)
