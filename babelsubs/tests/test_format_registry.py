4b831ac839f2edd10884ab753334139cb077a656
Ben Dean-Kawamura <ben@pculture.org>
2014-06-25 11:36:19 -0400

    Removed available formats test
    Part of the babelsubs design is to allow other modules to add formats
    and the test would fail whenever they did that.  In particular, it
    would fail when the amara modules were loaded alongside the babelsubs
    modules.

--- babelsubs/tests/test_format_registry.py
+++ babelsubs/tests/test_format_registry.py
@@ -7,11 +7,6 @@ from babelsubs.generators.base import discover as generator_discover
 
 
 class FormatRegistryTest(TestCase):
-    def test_available_formats(self):
-        actual_formats = sorted(get_available_formats())
-        expected_formats = sorted(['ass',  'srt', 'dfxp', 'ssa', 'sbv', 'txt', 'json', 'vtt', 'xml'])
-        self.assertEqual(actual_formats, expected_formats)
-
     def test_parsing_discover_lowercase(self):
         self.assertTrue(base.ParserList['srt'])
         self.assertTrue(base.ParserList['SRT'])
